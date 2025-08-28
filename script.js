634e51ccfd17fbbd75b02d4723ac975e1da60dcd
nana1268 <163775526+nana1268@users.noreply.github.com>
2025-08-28 23:01:00 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
